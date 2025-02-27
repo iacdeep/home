8c74c588e9de85a80434fbaa2bfa134bafc214c7
iacdeep <153907123+iacdeep@users.noreply.github.com>
2025-02-27 10:30:16 +0000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -12,8 +12,8 @@ export default function About() {
           <h2 className={utilStyles.headingLg}>About</h2>
 
           <p>
-            The IAC-DEEP group is dedicated to the development and application of machine learning (ML), deep learning (DL), and artificial intelligence (AI) techniques for the analysis and interpretation of astrophysical data and beyond. Our research covers a broad range of topics, from solar physics to galaxy evolution and cosmology expanding to bio-medicine projects.
-            Our group is engaged both in national and international level projects and collaboration. We have access to state-of-the-art computational facilities in Spain and internationally, enabling us to tackle complex astrophysical problems with cutting-edge machine learning techniques. Our team includes key members and leading contributors in major astronomical collaborations such as Euclid, DESI, and JWST, where we play an active role in data analysis and scientific exploration.
+            The IAC-DEEP group is dedicated to the development and application of machine learning (ML), deep learning (DL), and artificial intelligence (AI) techniques for the analysis and interpretation of astrophysical data and beyond. Our research covers a broad range of topics, from solar physics to galaxy evolution and cosmology expanding to bio-medicine projects. \n
+            Our group is engaged both in national and international level projects and collaboration. We have access to state-of-the-art computational facilities in Spain and internationally, enabling us to tackle complex astrophysical problems with cutting-edge machine learning techniques. Our team includes key members and leading contributors in major astronomical collaborations such as Euclid, DESI, and JWST, where we play an active role in data analysis and scientific exploration. \n
             By integrating advanced computational techniques with observational and theoretical astrophysics, we aim to optimize data processing, refine predictive models, and uncover new insights from large astronomical datasets and simulations, pushing the frontiers of modern astrophysics.
           </p>
         </section>
