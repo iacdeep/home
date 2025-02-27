bf629f7a648cd739dd2a33fdca1594ef1394bc29
iacdeep <153907123+iacdeep@users.noreply.github.com>
2025-02-27 09:55:59 +0000

    Update constants.js
    updating the list of members

--- utils/constants.js
+++ utils/constants.js
@@ -4,10 +4,18 @@ export const membersData = [
     { name: 'Carlos Westendorp', role: 'Faculty', researchArea: 'Multi Disciplinary ML applications', author: 'Westendorp, C.' },
     { name: 'Jesús Falcón-Barroso', role: 'Faculty', researchArea: 'Galaxy Evolution', author: 'Falcon-Barroso, J.' },
     { name: 'Ignacio Ferreras', role: 'Faculty', researchArea: 'Galaxy Evolution', author: 'Ferreras, I.'},
+    { name: 'Christopher Brook', role: 'Faculty', researchArea: 'Galaxy Evolution', author: 'Brook, C.' },
+    { name: 'Arianna Di Cintio', role: 'Faculty', researchArea: 'Galaxy Evolution', author: 'Di Cinto, A.' }
+    { name: 'Tobías Felipe García', role: 'PostDoc', researchArea: 'Solar Physics', author: 'García, T. F.' }
+    { name: 'Malgorzata Siudek', role: 'PostDoc', researchArea: 'Galaxy Evolution', author: 'Siudek, M.' }
     { name: 'Eirini Angeloudi', role: 'Grad Student', researchArea: 'Galaxy Evolution', author: 'Angeloudi, E.' },
-    { name: 'Regina Sarmiento', role: 'Grad Student', researchArea: 'Galaxy Evolution', author: 'Sarmiento, R.' },
     { name: 'Patricia Iglesias-Navarro', role: 'Grad Student', researchArea: 'Galaxy Evolution', author: 'Iglesias-Navarro, P.' },
     { name: 'Elena García Broock', role: 'Grad Student', researchArea: 'Solar Physics', author: 'Broock, E. G.' },
-  ];
+    { name: 'Eduardo Albuquerque Hartmann', role: 'Grad Student', researchArea: 'Galaxy Evolution', author: 'Hartmann, E. A.' },
+    { name: 'Marina Dunn', role: 'Grad Student', researchArea: 'Galaxy Evolution', author: 'Dunn, M.' },
+    { name: 'Jorge M. Sarrato Alós', role: 'Grad Student', researchArea: 'Galaxy Evolution', author: 'Sarrato, J. M.' }
+    { name: 'Andrés Vicente Arévalo', role: 'PostDoc', researchArea: 'Solar Physics', author: 'Arévalo, A. V.' }
+
+]
 
 export const authors = membersData.map(member => member.author);
